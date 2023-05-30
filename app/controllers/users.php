15dc24b15183428e995108c401269e501f0e17f7
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-05-30 19:36:23 +0300

    added the login code

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -8,6 +8,23 @@ $username = '';
 $email = '';
 $password = '';
 $passwordcof = '';
+$table ='users'; // initialise the table 
+
+function userlog($user){
+   // Log user in
+   $_SESSION['id'] = $user['id'];
+   $_SESSION['username'] = $user['username'];
+   $_SESSION['admin'] = $user['admin'];
+   $_SESSION['message'] = 'You are logged in';
+   $_SESSION['type'] = 'success';
+
+   if($_SESSION['admin']){
+     header('location: admin/dashboard/dashboard.php');
+   }else{
+     header('Location: index.php');
+   }
+   exit();
+}
 
  if (isset($_POST['submit-btn'])) {
     $errors = validateUser($_POST);
@@ -17,22 +34,10 @@ $passwordcof = '';
         $_POST['admin'] = 0;
         $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-        $user_id = create('users', $_POST);
-        $user = selectOne('users', ['id' => $user_id]);
+        $user_id = create($table, $_POST);
+        $user = selectOne($table, ['id' => $user_id]);
 
-        // Log user in
-        $_SESSION['id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
-        $_SESSION['admin'] = $user['admin'];
-        $_SESSION['message'] = 'You are logged in';
-        $_SESSION['type'] = 'success';
-
-        if($_SESSION['admin']){
-          header('location: admin/dashboard/dashboard.php');
-        }else{
-          header('Location: index.php');
-        }
-        exit();
+       userlog($user);
 
     } else {
         $username = $_POST['username'];
@@ -40,6 +45,27 @@ $passwordcof = '';
         $password = $_POST['password'];
         $passwordcof = $_POST['confirm_password'];
     }
+}
+
+//login 
+if(isset($_POST['login-btn'])){
+  
+  $errors = validateLogin($_POST);
+
+    if(count($errors)=== 0){
+      $user = selectOne($table, ['email' => $_POST['email']]);
+
+      if ($user && password_verify($_POST['password'], $user['password'])){
+
+        userlog($user);// call the user log function
+
+      } else {
+        array_push($errors, 'Wrong credentials');
+      }
+    }
+
+    $email = $_POST['email'];
+    $password =$_POST['password'];
 }
     
   
