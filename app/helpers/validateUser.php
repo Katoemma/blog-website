14fb2e9ceac37108eeb7ff18ae666cf7d3b57866
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-05-30 19:35:48 +0300

    added the validation for logging in

--- app/helpers/validateUser.php
+++ app/helpers/validateUser.php
@@ -23,3 +23,18 @@ function validateUser($user)
     }
     return $errors;
 }
+
+function validateLogin($user)
+{
+    $errors = array();
+    
+    if(empty($user['email'])){
+        array_push($errors, 'email is required');
+    }
+   
+    if(empty($user['password'])){
+        array_push($errors, 'Password is required');
+    }
+    
+    return $errors;
+}
