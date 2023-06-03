a89b30bb8c0d4290a9358dd6bf417c9649d560a2
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-06-03 12:12:48 +0300

    modified the cards by adding the swiperJS tool

--- index.php
+++ index.php
@@ -10,43 +10,44 @@
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
   <h2 class="text-3xl font-semibold text-black mb-8">Trending Posts</h2>
   <div class="overflow-x-auto">
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <!-- <div class=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"> -->
+    <swiper-container class="mySwiper" pagination="true" pagination-clickable="true" space-between="30" slides-per-view="3" autoplay-delay="2500" autoplay-disable-on-interaction="false">
       <!-- Card 1 -->
-      <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+      <swiper-slide class="bg-white shadow-lg rounded-lg overflow-hidden">
         <img src="pic.png" alt="Card 1" class="w-full">
         <div class="p-4">
           <h3 class="text-xl font-semibold mb-2">Title of Card 1</h3>
           <p class="text-gray-600 mb-2">Author: John Doe</p>
           <p class="text-gray-600">Date: May 18, 2023</p>
         </div>
-      </div>
+      </swiper-slide>
       <!-- Card 2 -->
-      <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+      <swiper-slide class="bg-white shadow-lg rounded-lg overflow-hidden">
         <img src="pic.png" alt="Card 2" class="w-full">
         <div class="p-4">
           <h3 class="text-xl font-semibold mb-2">Title of Card 2</h3>
           <p class="text-gray-600 mb-2">Author: Jane Smith</p>
           <p class="text-gray-600">Date: May 17, 2023</p>
         </div>
-      </div>
+      </swiper-slide>
       <!-- Card 3 -->
-      <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+      <swiper-slide class="bg-white shadow-lg rounded-lg overflow-hidden">
         <img src="pic.png" alt="Card 3" class="w-full">
         <div class="p-4">
           <h3 class="text-xl font-semibold mb-2">Title of Card 3</h3>
           <p class="text-gray-600 mb-2">Author: James Johnson</p>
           <p class="text-gray-600">Date: May 16, 2023</p>
         </div>
-      </div>
+      </swiper-slide>
       <!-- Card 4 -->
-      <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+      <swiper-slide class="bg-white shadow-lg rounded-lg overflow-hidden">
         <img src="pic.png" alt="Card 4" class="w-full">
         <div class="p-4">
           <h3 class="text-xl font-semibold mb-2">Title of Card 4</h3>
           <p class="text-gray-600 mb-2">Author: Emily Brown</p>
           <p class="text-gray-600">Date: May 15, 2023</p>
         </div>
-      </div>
+      </swiper-slide>
       <!-- Add more cards as needed -->
     </div>
   </div>
@@ -54,9 +55,9 @@
 
 
     <!-- recent posts section -->
-    <div class="bg-gray-100 py-12">
+<div class="bg-gray-100 py-12">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="grid grid-cols-1 lg:grid-cols-4 gap-6">
+    <div class="flex flex-col-reverse  md:flex-row gap-6">
       <!-- Posts Div -->
       <div class="lg:col-span-3">
         <!-- Post 1 -->
@@ -113,8 +114,30 @@
 </div>
 
 <!-- footer -->
+
 <?php include 'include/footer.php'?>
 
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+<script>
+    function changeElementAttribute(mediaQuery) {
+      var element = document.querySelector('.mySwiper');
+      if (mediaQuery.matches) {
+        // Change attribute when media query matches
+        element.setAttribute('slides-per-view','1');
+      } else {
+        // Revert attribute when media query doesn't match
+        element.setAttribute('slides-per-view','3');
+      }
+    }
+
+    var mediaQuery = window.matchMedia('(max-width: 768px)');
+
+    // Call the function on page load
+    changeElementAttribute(mediaQuery);
+
+    // Call the function whenever the screen size changes
+    mediaQuery.addEventListener('change', changeElementAttribute);
+  </script>
 
 </body>
 </html>
